03dc92578c51ee117673ac111d6e63115d24d767
MohamedKandel <73765302+MohamedKandel@users.noreply.github.com>
2022-03-04 17:14:30 +0200

    Update AlarmReceiver.java

--- app/src/main/java/com/mkandeel/dalelelemanupdate/HelperClasses/AlarmReceiver.java
+++ app/src/main/java/com/mkandeel/dalelelemanupdate/HelperClasses/AlarmReceiver.java
@@ -18,26 +18,31 @@ public class AlarmReceiver extends BroadcastReceiver {
         String Notif_message = "حي على الصلاة";
         if (intent.getExtras() != null) {
             int index = intent.getExtras().getInt("index");
-            switch (index) {
-                case 0:
-                    Notif_title = "حان الآن موعد آذان الفجر";
-                    Notif_message = "الصلاة خير من النوم";
-                    break;
-                case 1:
-                    Notif_title = "حان الآن موعد آذان الظهر";
-                    break;
-                case 2:
-                    Notif_title = "حان الآن موعد آذان العصر";
-                    break;
-                case 3:
-                    Notif_title = "حان الآن موعد آذان المغرب";
-                    break;
-                case 4:
-                    Notif_title = "حان الآن موعد آذان العشاء";
-                    break;
+            if (index > 5) {
+                switch (index) {
+                    case 0:
+                        Notif_title = "حان الآن موعد آذان الفجر";
+                        Notif_message = "الصلاة خير من النوم";
+                        break;
+                    case 1:
+                        Notif_title = "حان الآن موعد آذان الظهر";
+                        break;
+                    case 2:
+                        Notif_title = "حان الآن موعد آذان العصر";
+                        break;
+                    case 3:
+                        Notif_title = "حان الآن موعد آذان المغرب";
+                        break;
+                    case 4:
+                        Notif_title = "حان الآن موعد آذان العشاء";
+                        break;
+                }
+                tools.ShowNotification(Notif_title, Notif_message, praying_time.class);
+            } else {
+                Notif_title = "الثلث الاخير من الليل";
+                Notif_message = "اقم الليل واذكر الله";
+                tools.QyamNotification(Notif_title,Notif_message,praying_time.class);
             }
         }
-        tools.ShowNotification(Notif_title, Notif_message, praying_time.class);
-        //wakeLock.release();
     }
 }
